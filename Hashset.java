453226509b8b13125487e3b29415e8299441ce22
-redacted- <ljeanrenz@gmail.com>
2024-11-19 21:00:47 +0800

    HASHSET ---BACKUP---

---
+++ Hashset.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hashset;
+
+/**
+ *
+ * @author Jeanrenz
+ */
+import java.util.HashSet;
+public class Hashset {
+
+    
+    public static void main(String[] args) {
+        HashSet<String> branch = new HashSet<>();
+        
+        branch.add("Makati");
+        branch.add("Manila");
+        branch.add("Alabang");
+        System.out.println("--- HASHSET ---");
+        System.out.println("FEU Branches: " + branch);
+        branch.add("Cavite");
+        System.out.println("After adding a branch:  " + branch);
+        branch.contains("Manila");
+        System.out.println("Does it contain manila branch?: " + branch.contains("Manila"));
+        System.out.println("Number of branches: " + branch.size());
+        
+    }
+}
